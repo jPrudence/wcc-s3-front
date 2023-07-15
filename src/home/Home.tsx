e40defc53467c0d74ab6cada4bd01485b9e75783
Sahobisoa Rolland <andrianiainarolland123@gmail.com>
2023-07-15 23:01:24 +0300

    first commit 'no inspi man'

---
+++ src/home/Home.tsx
@@ -0,0 +1,18 @@
+import "./Home.scss"
+
+const Home = () => {
+    return (
+        <h2 className="animated-letter">
+            <h4 className="animated-letter" id="A">A</h4>
+            <h4 className="animated-letter" id="N" >N</h4>
+            <h4 className="animated-letter" id="S">S</h4>
+            <h4 className="animated-letter" id="A2">A</h4>
+            <h4 className="animated-letter" id="R">R</h4>
+            <h4 className="animated-letter" id="O">O</h4>
+            <h4 className="animated-letter" id="L">L</h4>
+            <h4 className="animated-letter" id="L2">L</h4>
+        </h2>
+    )
+}
+
+export default Home
